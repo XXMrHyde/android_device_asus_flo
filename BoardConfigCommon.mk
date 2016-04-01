82e75694490085c75505809860e55a7221e9c0f3
XXMrHyde <xxmrhyde@gmx.co.uk>
2016-04-02 01:11:17 +0200

    Fix inline kernel build

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -32,7 +32,7 @@ BOARD_KERNEL_CMDLINE += vmalloc=340M
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02000000
 
 # Kernel inline build
-TARGET_KERNEL_CUSTOM_TOOLCHAIN := arm-eabi-4.6
+KERNEL_TOOLCHAIN := "$(ANDROID_BUILD_TOP)/prebuilts/gcc/$(strip $(HOST_OS))-x86/arm/arm-eabi-4.6/bin/"
 TARGET_KERNEL_SOURCE := kernel/msm/flo
 TARGET_KERNEL_CONFIG := flo_defconfig
 TARGET_VARIANT_CONFIG := flo_defconfig
